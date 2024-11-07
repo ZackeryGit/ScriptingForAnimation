ecfbb06653708a53600a975f0254390ea75fdcf5
Zackery Elmer <Zackeryelmer@comcast.net>
2024-11-07 08:20:58 -0700

    Completed Rigtool

---
+++ Python/Unit 7/rigtool.py
@@ -0,0 +1,16 @@
+import maya.cmds as c
+
+selection = c.ls(selection=True)
+print(selection)
+for item in selection:
+    group = c.group(empty = True, name = item + "_Grp")
+    # Get Attributes
+    translation = c.xform(item, query = True, translation=True)
+    rotation = c.xform(item, query = True, rotation=True)
+    # Set Attributes
+    c.xform(group, translation = translation)
+    c.xform(group, rotation = rotation)
+    c.parent(item, group)
+    # Freeze Transforms
+    c.makeIdentity(item, scale=True, translate=True, rotate=True, normal=True)
+    
